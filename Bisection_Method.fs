bb306e4d5d483a6d027d583f37acfcb904d3598e
eegodinez <edgarenrique.godinez@gmail.com>
2018-05-18 11:23:06 -0600

    Update Bisection_Method.fs

--- Bisection_Method.fs
+++ Bisection_Method.fs
@@ -4,20 +4,20 @@ let rec Bisection_Method (f:float -> float) (a:float) (b:float) (tol:float) (ite
         printfn "No root inside the interval! Returning 0."
         0.0
     else
-        let x_new = (a+b)/2.0
+        let m = (a+b)/2.0
         //if our approximation is not good enough, recursively iterate until it is.
         if (b-a >= tol ) then
-            printfn "iternum: %d | x_new: %f" iternum x_new
+            printfn "iternum: %d | midpoint: %f" iternum m
             (*if the sign of the function at the midpoint is equal to the sign of the function at our current point,
             discard the lower half of the interval 
             *)
-            if sign(f a) * sign(f x_new) > 0 then
-                Bisection_Method f x_new b tol (iternum+1)
+            if sign(f a) * sign(f m) > 0 then
+                Bisection_Method f m b tol (iternum+1)
             else
-                Bisection_Method f a x_new tol (iternum+1)
+                Bisection_Method f a m tol (iternum+1)
         else
-            printfn "\nfinal number of iterations: %d | root value: %f" iternum x_new
-            x_new
+            printfn "\nfinal number of iterations: %d | root value: %f" iternum m
+            m
 
 //sample function: 2x^2 - 11x + 5
 let sample_function x:float = 2.0*x**2.0 - 11.0*x + 5.0
